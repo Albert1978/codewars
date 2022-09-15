086ddef476b9511a2b3144141dd1f2c28f0f52e0
Albert <bert1978@rambler.ru>
2022-09-15 16:58:10 +0500

    Решение задачи: преобразование массива слов в предложение

--- index.js
+++ index.js
@@ -10,18 +10,35 @@
 }
 console.log(isSquare(26)) */
 
-function getCount(str) {
+// function getCount(str) {
 
-	let strArr = str.toLowerCase().split('');
-	const vowels = ['a', 'e', 'i', 'o', 'u'];
-	let count = 0;
+// 	let strArr = str.toLowerCase().split('');
+// 	const vowels = ['a', 'e', 'i', 'o', 'u'];
+// 	let count = 0;
 
-	for (char of strArr) {
-		if (vowels.includes(char)) {
-			count++;
+// 	for (char of strArr) {
+// 		if (vowels.includes(char)) {
+// 			count++;
+// 		}
+// 	}
+// 	return count;
+// }
+
+// console.log(getCount('AfgfOhgi ghOO')); 
+
+function smash(words) {
+	console.log(words)
+	let str = "";
+
+	words.forEach((item, index, array) => {
+		if (index < array.length - 1) {
+			str += `${item} `
 		}
-	}
-	return count;
-}
-
-console.log(getCount('AfgfOhgi ghOO')); 
+		else {
+			str += `${item}`
+		}
+	})
+	return str
+};
+let worlds = ['hello', 'world', 'this', 'is', 'great', '!'];
+console.log(smash(worlds));
