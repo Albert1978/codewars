9616a60c893f0c85eeee0a3e5abd4f66bb159144
Albert <bert1978@rambler.ru>
2022-09-19 11:43:26 +0500

    Зфвфча на преобразование ДНК в РНК

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
 }
 console.log(isSquare(26)) */
 
+//=================================================================//
+
 // function getCount(str) {
 
 // 	let strArr = str.toLowerCase().split('');
@@ -26,19 +28,92 @@ console.log(isSquare(26)) */
 
 // console.log(getCount('AfgfOhgi ghOO')); 
 
-function smash(words) {
-	console.log(words)
-	let str = "";
+//=================================================================//
 
-	words.forEach((item, index, array) => {
-		if (index < array.length - 1) {
-			str += `${item} `
+// function smash(words) {
+// 	console.log(words)
+// 	let str = "";
+
+// 	words.forEach((item, index, array) => {
+// 		if (index < array.length - 1) {
+// 			str += `${item} `
+// 		}
+// 		else {
+// 			str += `${item}`
+// 		}
+// 	})
+// 	return str
+// };
+// let worlds = ['hello', 'world', 'this', 'is', 'great', '!'];
+// console.log(smash(worlds));
+
+//=================================================================//
+
+// function lovefunc(flower1, flower2) {
+// 	if ((flower1 + flower2) % 2 == 1) {
+// 		return true
+// 	}
+// 	else {
+// 		return false
+// 	}
+// 	// moment of truth
+// }
+
+// console.log(lovefunc(3, 4)) 
+
+//=================================================================//
+
+// function countPositivesSumNegatives(input) {
+
+// 	let sum = 0;
+// 	let count = 0;
+
+// 	if (input.some(currentValue => currentValue == null)) {
+// 		return [];
+// 	}
+// 	else {
+// 		for (let i = 0; i < input.length; i++) {
+// 			if (input[i] < 0) {
+// 				sum = sum + input[i];
+// 			}
+// 			else if (input[i] > 0) {
+// 				count++;
+// 			}
+// 		}
+
+// 	}
+// 	if (sum == 0 && count == 0) {
+// 		return []
+// 	}
+// 	else {
+// 		return [count, sum];
+// 	}
+// }
+
+// console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, null, 10, -11, -12, -13, -14, -15]));
+// console.log(countPositivesSumNegatives([]));
+// console.log(null == null);
+// console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+// console.log([null] == null);
+
+//=================================================================//
+
+function DNAtoRNA(dna) {
+
+	let str = '';
+
+	dna.toUpperCase();
+
+	for (let char of dna) {
+		if (char == 'T') {
+			str += 'U'
 		}
 		else {
-			str += `${item}`
+			str += char
 		}
-	})
-	return str
-};
-let worlds = ['hello', 'world', 'this', 'is', 'great', '!'];
-console.log(smash(worlds));
+	}
+	return str;
+	// create a function which returns an RNA sequence from the given DNA sequence
+}
+
+console.log(DNAtoRNA('TTTT'));
